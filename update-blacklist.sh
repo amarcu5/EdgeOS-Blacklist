3838ac32c247b1873fed246deb0f7aa35d5857ee
amarcu5 <adampetermarcus@gmail.com>
2017-03-11 04:04:04 +0000

    Formatting and bug fixes
    Adjusted formatting, blacklist suggestions now include Team Cymru bogon
    list, added maxelem configuration option, fixed a bug with duplicate
    entries, and removed unnecessary code

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -1,15 +1,19 @@
 #!/bin/vbash
 
 BLACKLIST=(
+	#Suggestions
     "https://www.spamhaus.org/drop/drop.txt"
     "https://www.spamhaus.org/drop/edrop.txt"
     "https://www.spamhaus.org/drop/dropv6.txt"
     #"http://www.myip.ms/files/blacklist/general/latest_blacklist.txt"
-    #"http://lists.blocklist.de/lists/all.txt"
+    #"https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt"
+    #"https://www.team-cymru.org/Services/Bogons/fullbogons-ipv6.txt"
     #"https://www.dshield.org/block.txt"
     #"http://www.openbl.org/lists/base_30days.txt"
     #"http://malc0de.com/bl/IP_Blacklist.txt"
+    #"http://lists.blocklist.de/lists/all.txt"
     #"http://blocklist.greensnow.co/greensnow.txt"
+    #Examples
     #"1.1.1.1"
     #"1.1.0.0/16"
     #"a:a:a:a:a:a:a:a"
@@ -17,15 +21,20 @@ BLACKLIST=(
 )
 
 WHITELIST=(
-	"10.0.0.0/8"
-	"172.16.0.0/12"
-	"192.168.0.0/16"
-	"fc00::/7"
-	#"http://some.site.containing/a_whitelist.txt"
+	#Suggestions
+    "10.0.0.0/8"
+    "172.16.0.0/12"
+    "192.168.0.0/16"
+    "fc00::/7"
+    #Examples
+    #"http://some.site.containing/a_whitelist.txt"
 )
 
 NETGROUP="BLACKLIST_DROP"
 NETGROUPv6="BLACKLIST_DROPv6"
+
+
+MAXELEM=131072
 
 NEWGROUP="$NETGROUP-$$"
 NEWGROUPv6="$NETGROUPv6-$$"
@@ -38,10 +47,8 @@ trap 'rm "$TMPFILE"' EXIT
 {
 	
 cat <<EOF
-create $NETGROUP -exist hash:net
-create $NETGROUPv6 -exist hash:net family inet6
-create $NEWGROUP hash:net
-create $NEWGROUPv6 hash:net family inet6
+create $NEWGROUP hash:net maxelem $MAXELEM
+create $NEWGROUPv6 hash:net family inet6 maxelem $MAXELEM
 EOF
 
 function processlist {
@@ -49,10 +56,10 @@ function processlist {
 	do
 		if [[ $LIST == http* ]]
 		then
-			{ error=$({ curl -fsS --tr-encoding "$LIST" | tee >(egrep -o '((25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])\.){3}(25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])(\/(3[0-2]|[12]?[[:digit:]]))?([[:space:]-]+((25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])\.){3}(25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]]))?' | sed -e 's/[[:space:]-]\+/-/g' -e "s/.*/-A $NEWGROUP \0 $2/") >(egrep -o '([[:xdigit:]]{1,4}:){1,7}:|([[:xdigit:]]{1,4}:){1,5}(:[[:xdigit:]]{1,4}){1,2}|([[:xdigit:]]{1,4}:){1,4}(:[[:xdigit:]]{1,4}){1,3}|([[:xdigit:]]{1,4}:){1,3}(:[[:xdigit:]]{1,4}){1,4}|([[:xdigit:]]{1,4}:){1,2}(:[[:xdigit:]]{1,4}){1,5}|[[:xdigit:]]{1,4}:(:[[:xdigit:]]{1,4}){1,6}|:((:[[:xdigit:]]{1,4}){1,7}|:)|fe80:(:[[:xdigit:]]{0,4}){0,4}%[[:alnum:]]+|(([[:xdigit:]]{1,4}:){7}|([[:xdigit:]]{1,4}:){1,6}:)[[:xdigit:]]{1,4}|(::(f{4}(:0{1,4})?:)?|([[:xdigit:]]{1,4}:){1,4}:)((25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])\.){3}(25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])(/(6[0-4]|[1-5]?[[:digit:]]))?' | sed "s/.*/-A $NEWGROUPv6 \0 $2/") >/dev/null | cat; } 2>&1 1>&$out); } {out}>&1
+			{ error=$({ curl -fsS --tr-encoding "$LIST" | tee >(egrep -o '((25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])\.){3}(25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])(\/(3[0-2]|[12]?[[:digit:]]))?([[:space:]-]+((25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])\.){3}(25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]]))?' | sed -e 's/[[:space:]-]\+/-/g' -e "s/.*/-A -! $NEWGROUP \0 $2/") >(egrep -o '([[:xdigit:]]{1,4}:){1,7}:|([[:xdigit:]]{1,4}:){1,5}(:[[:xdigit:]]{1,4}){1,2}|([[:xdigit:]]{1,4}:){1,4}(:[[:xdigit:]]{1,4}){1,3}|([[:xdigit:]]{1,4}:){1,3}(:[[:xdigit:]]{1,4}){1,4}|([[:xdigit:]]{1,4}:){1,2}(:[[:xdigit:]]{1,4}){1,5}|[[:xdigit:]]{1,4}:(:[[:xdigit:]]{1,4}){1,6}|:((:[[:xdigit:]]{1,4}){1,7}|:)|fe80:(:[[:xdigit:]]{0,4}){0,4}%[[:alnum:]]+|(([[:xdigit:]]{1,4}:){7}|([[:xdigit:]]{1,4}:){1,6}:)[[:xdigit:]]{1,4}|(::(f{4}(:0{1,4})?:)?|([[:xdigit:]]{1,4}:){1,4}:)((25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])\.){3}(25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])(/(6[0-4]|[1-5]?[[:digit:]]))?' | sed "s/.*/-A -! $NEWGROUPv6 \0 $2/") >/dev/null | cat; } 2>&1 1>&$out); } {out}>&1
 			[[ ! -z "$error" ]] && { logger -s "Blacklist update failed (Unable to access '$LIST')"; exit 1; }
 		else
-			[[ $LIST == *:* ]] && sed "s/.*/-A $NEWGROUPv6 \0 $2/" <<< $LIST || sed "s/.*/-A $NEWGROUP \0 $2/" <<< $LIST
+			[[ $LIST == *:* ]] && sed "s/.*/-A -! $NEWGROUPv6 \0 $2/" <<< $LIST || sed "s/.*/-A -! $NEWGROUP \0 $2/" <<< $LIST
 		fi
 	done
 }
