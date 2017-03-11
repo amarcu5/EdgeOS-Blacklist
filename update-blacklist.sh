bdef0f06bf08c953a5e25722471727c9907e7c07
amarcu5 <adampetermarcus@gmail.com>
2017-03-11 03:21:54 +0000

    Added whitelist support and improves error resiliency
    Added whitelist, support for hard coded IP addresses/ranges, basic
    logging, improves IPv6 compatibility, and better error resiliency
    ensuring no partial list updates

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -1,6 +1,6 @@
 #!/bin/vbash
 
-BLACKLISTS=(
+BLACKLIST=(
     "https://www.spamhaus.org/drop/drop.txt"
     "https://www.spamhaus.org/drop/edrop.txt"
     "https://www.spamhaus.org/drop/dropv6.txt"
@@ -10,6 +10,18 @@ BLACKLISTS=(
     #"http://www.openbl.org/lists/base_30days.txt"
     #"http://malc0de.com/bl/IP_Blacklist.txt"
     #"http://blocklist.greensnow.co/greensnow.txt"
+    #"1.1.1.1"
+    #"1.1.0.0/16"
+    #"a:a:a:a:a:a:a:a"
+    #"a:a:a:a:a:a::/48"
+)
+
+WHITELIST=(
+	"10.0.0.0/8"
+	"172.16.0.0/12"
+	"192.168.0.0/16"
+	"fc00::/7"
+	#"http://some.site.containing/a_whitelist.txt"
 )
 
 NETGROUP="BLACKLIST_DROP"
@@ -17,7 +29,9 @@ NETGROUPv6="BLACKLIST_DROPv6"
 
 NEWGROUP="$NETGROUP-$$"
 NEWGROUPv6="$NETGROUPv6-$$"
+
 TMPFILE="/tmp/block-$$"
+trap 'rm "$TMPFILE"' EXIT
 
 [ -L /dev/fd ] || (sudo ln -s /proc/self/fd /dev/fd)
 
@@ -30,10 +44,20 @@ create $NEWGROUP hash:net
 create $NEWGROUPv6 hash:net family inet6
 EOF
 
-for BLACKLIST in "${BLACKLISTS[@]}"
-do
-	curl -s --tr-encoding "$BLACKLIST" | tee >(egrep -o '([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3}(\/[[:digit:]]{1,2})?([[:space:]-]+([[:digit:]]{1,3}\.){3}[[:digit:]]{1,3})?' | sed -e "s/[[:space:]-]\+/-/g" -e "s/^/-A $NEWGROUP /") >(egrep -o '(([[:xdigit:]]{1,4}:){1,7}:|([[:xdigit:]]{1,4}:){1,5}(:[[:xdigit:]]{1,4}){1,2}|([[:xdigit:]]{1,4}:){1,4}(:[[:xdigit:]]{1,4}){1,3}|([[:xdigit:]]{1,4}:){1,3}(:[[:xdigit:]]{1,4}){1,4}|([[:xdigit:]]{1,4}:){1,2}(:[[:xdigit:]]{1,4}){1,5}|[[:xdigit:]]{1,4}:(:[[:xdigit:]]{1,4}){1,6}|:((:[[:xdigit:]]{1,4}){1,7}|:)|(([[:xdigit:]]{1,4}:){7}|([[:xdigit:]]{1,4}:){1,6}:)[[:xdigit:]]{1,4})(/[[:digit:]]{1,2})?' | sed -e "s/^/-A $NEWGROUPv6 /") >/dev/null | cat
-done
+function processlist {
+	for LIST in "${!1}"
+	do
+		if [[ $LIST == http* ]]
+		then
+			{ error=$({ curl -fsS --tr-encoding "$LIST" | tee >(egrep -o '((25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])\.){3}(25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])(\/(3[0-2]|[12]?[[:digit:]]))?([[:space:]-]+((25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])\.){3}(25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]]))?' | sed -e 's/[[:space:]-]\+/-/g' -e "s/.*/-A $NEWGROUP \0 $2/") >(egrep -o '([[:xdigit:]]{1,4}:){1,7}:|([[:xdigit:]]{1,4}:){1,5}(:[[:xdigit:]]{1,4}){1,2}|([[:xdigit:]]{1,4}:){1,4}(:[[:xdigit:]]{1,4}){1,3}|([[:xdigit:]]{1,4}:){1,3}(:[[:xdigit:]]{1,4}){1,4}|([[:xdigit:]]{1,4}:){1,2}(:[[:xdigit:]]{1,4}){1,5}|[[:xdigit:]]{1,4}:(:[[:xdigit:]]{1,4}){1,6}|:((:[[:xdigit:]]{1,4}){1,7}|:)|fe80:(:[[:xdigit:]]{0,4}){0,4}%[[:alnum:]]+|(([[:xdigit:]]{1,4}:){7}|([[:xdigit:]]{1,4}:){1,6}:)[[:xdigit:]]{1,4}|(::(f{4}(:0{1,4})?:)?|([[:xdigit:]]{1,4}:){1,4}:)((25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])\.){3}(25[0-5]|(2[0-4]|1?[[:digit:]])?[[:digit:]])(/(6[0-4]|[1-5]?[[:digit:]]))?' | sed "s/.*/-A $NEWGROUPv6 \0 $2/") >/dev/null | cat; } 2>&1 1>&$out); } {out}>&1
+			[[ ! -z "$error" ]] && { logger -s "Blacklist update failed (Unable to access '$LIST')"; exit 1; }
+		else
+			[[ $LIST == *:* ]] && sed "s/.*/-A $NEWGROUPv6 \0 $2/" <<< $LIST || sed "s/.*/-A $NEWGROUP \0 $2/" <<< $LIST
+		fi
+	done
+}
+processlist BLACKLIST[@]
+processlist WHITELIST[@] "nomatch"
 
 cat <<EOF
 swap $NEWGROUP $NETGROUP
@@ -44,6 +68,6 @@ EOF
 
 } >"$TMPFILE"
 
-sudo ipset -file "$TMPFILE" restore
-rm "$TMPFILE"
-
+result=$(sudo ipset -file "$TMPFILE" restore 2>&1)
+[[ ! -z "$result" ]] && { logger -s "Blacklist update failed"; exit 1; }
+logger -s "Blacklist update successful"
